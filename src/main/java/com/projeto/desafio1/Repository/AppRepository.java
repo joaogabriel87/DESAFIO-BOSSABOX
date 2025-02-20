ff1153ac6c78608fc809ce54924e477783985e7d
joaogabriel87 <joagabriel9@gmail.com>
2025-02-19 22:31:39 -0300

    dev: adicionando o repository do projeto

---
+++ src/main/java/com/projeto/desafio1/Repository/AppRepository.java
@@ -0,0 +1,23 @@
+package com.projeto.desafio1.Repository;
+
+import com.projeto.desafio1.model.AppEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface AppRepository extends JpaRepository<AppEntity, Long> {
+
+    List<AppEntity> findAllBy();
+
+    List<AppEntity> findByTagsIn(List<String> tags);
+
+    default AppEntity saveApp(AppEntity app){
+        return save(app);
+    }
+
+     default void  deleteApp(AppEntity app){
+        delete(app);
+     }
+}
+
+
--- src/main/java/com/projeto/desafio1/model/AppEntity.java
+++ src/main/java/com/projeto/desafio1/model/AppEntity.java
@@ -1,4 +1,4 @@
-package com.projeto.desafio1.Entity;
+package com.projeto.desafio1.model;
 
 import jakarta.persistence.*;
 import lombok.Data;
